f972a856d5f817b73ad07f431893b125a14ae8bb
502-PC-lida <njulida@outlook.com>
2021-01-30 14:48:13 +0800

    await has to be used with async function

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -17,16 +17,12 @@ Office.onReady(info => {
     }
 
     document.getElementById("create-table").onclick = createTable;
-    document.getElementById("filter-table").onclick = filterTable;
-    document.getElementById("sort-table").onclick = sortTable;
     document.getElementById("warder-analysis").onclick = warderAnalysis;
 
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    
   }
 });
-
 
 function createTable() {
   Excel.run(function (context) {
@@ -59,71 +55,23 @@ function createTable() {
   });
 }
 
-function filterTable() {
-  Excel.run(function (context) {
-
-    var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-    var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
-    var categoryFilter = expensesTable.columns.getItem('Category').filter;
-    categoryFilter.applyValuesFilter(['Education', 'Groceries']);
-
-    return context.sync();
-  })
-  .catch(function (error) {
-    console.log("Error: " +error);
-    if (error instanceof OfficeExtension.Error) {
-      console.log("Debug info: " + JSON.stringify(error.debugInfo));
-    }
-  });
-}
-
-function sortTable() {
-  Excel.run(function (context) {
-
-    var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-    var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
-    var sortFields = [
-        {
-            key: 1,            // Merchant column
-            ascending: false,
-        }
-    ];
-    
-    expensesTable.sort.apply(sortFields);
-
-    return context.sync();
-  })
-  .catch(function (error) {
-      console.log("Error: " + error);
-      if (error instanceof OfficeExtension.Error) {
-          console.log("Debug info: " + JSON.stringify(error.debugInfo));
-      }
-  });
-}
-
 function warderAnalysis() {
-  Excel.run(function (context) {
+  Excel.run(async function (context) {
     //mini test
     //why console.log cannot be seen, it must be somewhere???
-
+    console.log("mini test");
     var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-    var usedRange = currentWorksheet.getUsedRangeOrNullObject(false);
+    var logTable = currentWorksheet.tables.add("A10:B10", true /* hasHeaders */);
+    logTable.getHeaderRowRange().values = [["log", "content"]];
+
+    var usedRange = currentWorksheet.getUsedRange();
     usedRange.load("cellCount");
     await context.sync();
-    // if (usedRange.isNullObject) {
-    //   console.log("[E] worksheet is blank.");
-    // }
-
-    var logTable = currentWorksheet.tables.add("A10:B10", true /* hasHeaders */);
-    logTable.name = "LogTable";
-    logTable.getHeaderRowRange().values = [["log", "content"]];
+    
     logTable.rows.add(null /* add at the end */, [
       [1, "range contains " + 0 + " cells"],
-      [2, 888],
       [3, "range contains " + usedRange.cellCount + " cells"]
     ]);
-    logTable.getRange().format.autofitColumns();
-    logTable.getRange().format.autofitRows();
 
     //begin
 
@@ -138,9 +86,24 @@ function warderAnalysis() {
     return context.sync();
   })
   .catch(function (error) {
-      console.log("Error: " + error);
-      if (error instanceof OfficeExtension.Error) {
-          console.log("Debug info: " + JSON.stringify(error.debugInfo));
-      }
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  });
+}
+
+function getValues() {
+  Excel.run(async function (context) {
+    var sheet = context.workbook.worksheets.getActiveWorksheet();
+    var range = sheet.getRange("A2:D8");
+    range.load("cellCount");
+    range.load("values");
+
+    await context.sync();
+    console.log(JSON.stringify(range.values, null, 4));
+  })
+  .catch(function (error) {
+    //nothing
   });
 }
